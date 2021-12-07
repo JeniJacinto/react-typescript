429fa9a27d0bb397e9bde033742c6c63f185c49b
TsehelnykAlona <tsehelnykalona@gmail.com>
2021-12-07 11:55:30 +0800

    project oK

--- src/components/posts/item.tsx
+++ src/components/posts/item.tsx
@@ -12,40 +12,40 @@ const Item: React.FC = () => {
 
     const getPost = (id: string | undefined) => {
         PostApi.get(id)
-         .then((response: any) => {
-             setPost(response.data);
-             console.log(response.data);
-         })
-         .catch((e: Error) => {
-             console.log(e);
-         });
+            .then((response: any) => {
+                setPost(response.data);
+                console.log(response.data);
+            })
+            .catch((e: Error) => {
+                console.log(e);
+            });
     };
-    
+
     return (
         <>
             {post && (
-            <div>
-                <h2 className="p-2 bg-info text-white text-center">Post Item ID: { id }</h2><br />
-                <table className="table table-striped">
-                    <thead>
-                        <th>UserId</th>
-                        <th>ID</th>
-                        <th>Title</th>
-                        <th>Body</th>
-                    </thead>
-                    <tbody>
+                <div>
+                    <h2 className="p-2 bg-info text-white text-center">Post Item ID: {id}</h2><br />
+                    <table className="table table-striped">
+                        <thead>
+                            <th>UserId</th>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Body</th>
+                        </thead>
+                        <tbody>
                             <tr>
-                                <td>{ post.userId }</td>
-                                <td>{ post.id }</td>
-                                <td>{ post.title }</td>
-                                <td>{ post.body }</td>
+                                <td>{post.userId}</td>
+                                <td>{post.id}</td>
+                                <td>{post.title}</td>
+                                <td>{post.body}</td>
                             </tr>
-                    </tbody>
-                </table>
-                <div className="d-grid">
-                    <a href="/posts" className="btn btn-primary btn-block">Return Post List</a>
+                        </tbody>
+                    </table>
+                    <div className="d-grid">
+                        <a href="/posts" className="btn btn-primary btn-block">Return Post List</a>
+                    </div>
                 </div>
-            </div>
             )}
         </>
     )
